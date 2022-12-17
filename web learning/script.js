c7ad17c83573cc3d8c9488eb0f5309228c97901e
07nainankit <ankitnain_ec21a16_07@dtu.ac.in>
2022-12-17 17:26:19 +0530

    day 2 javascript
    pretty basic

--- web learning/script.js
+++ web learning/script.js
@@ -1,2 +1,42 @@
-// day one 16       24 hrs let's see 
+// day one 16       24 hrs let's see
 //babel (old browsers compatiblity)
+
+//to avoid mistakes
+"use strict";
+
+console.log("hellow world");
+
+//' ' `(1) can be used
+
+//variables //naming of variables (rules $ can be used)
+
+var $value_1is = 2;
+console.log($value_1is ** 3); //for power **
+
+//cammel case using in js firstName, FistName for class
+
+//let prefered over var
+let first = "ankit";
+console.log(first);
+console.log(first.length);
+console.log(first[3]);
+
+first = "     Aprit       ";
+console.log(first);
+console.log(first.length);
+console.log(first[3]);
+
+first = first.trim();
+console.log(first);
+console.log(first.length);
+console.log(first[3]);
+//some operations on strings
+console.log(first.toUpperCase());
+console.log(first.toLowerCase());
+console.log(first.slice(1, 4)); //1to end (1)
+
+const age = 3;
+// name="arpit";//can't be changed
+console.log(age);
+
+// 1 10 leave
